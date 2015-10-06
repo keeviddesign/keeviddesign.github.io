580737df34f7c05d3708fe90b933647fdabaf972
keeviddesign <keeviddesign@users.noreply.github.com>
2015-10-05 23:16:50 -0500

    Revert "Revert "version2.1""
    This reverts commit 0809bb2be96c2657ef65c726031482c45f0125a1.

---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+$( document ).ready(function( $ ) {
+
+		$( '#page-portfolio' ).sliderPro({
+			width: 960,
+			height: 500,
+			fade: true,
+			arrows: true,
+			buttons: false,
+			fullScreen: true,
+			shuffle: true,
+			smallSize: 500,
+			mediumSize: 1000,
+			largeSize: 3000,
+			thumbnailArrows: true,
+			autoplay: false
+		});
+	});
